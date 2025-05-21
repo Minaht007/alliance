8e6163e5c16ed43fa2b0e0d1e7f4e6af05371262
SerheyPopov <serheypopov1994@gmail.com>
2025-05-21 23:53:06 +0300

    21.05.2025 user profile

--- src/component/weapons/weapons.jsx
+++ src/component/weapons/weapons.jsx
@@ -9,47 +9,42 @@ const Weapons = () => {
   const [modalOpen, setModalOpen] = useState(false);
   const [showClearImage, setShowClearImage] = useState(false);
 
-  const handleOpenModal = () => {
-    setModalOpen(true);
-  };
+  // const handleOpenModal = () => {
+  //   setModalOpen(true);
+  // };
 
-  const handleUnlockTank = () => {
-    setShowClearImage(true);
-  };
+  // const handleUnlockTank = () => {
+  //   setShowClearImage(true);
+  // };
 
   return (
-    <div
-      className={styles.wrapper}
-      style={{ backgroundImage: `url(${bgImage})` }}
-    >
-      <ModalWind title={"Танк"} isOpen={true} onClose={() => setModalOpen(false)} style={"marginTop: '500px'"}>
-		
-        <div style={{ marginTop: "20px" }}>
-          <img
-            src={showClearImage ? tank : blurTank}
-            alt="tank"
-            style={{ width: "255px", height: "144px" }}
-          />
+		<div className={styles.wrapper} style={{ backgroundImage: `url(${bgImage})` }}>
+			<ModalWind
+				title={"Танк"}
+				isOpen={modalOpen}
+				onClose={() => setModalOpen(false)}
+				style={"marginTop: '500px'"}
+			>
+				<div style={{ marginTop: "20px" }}>
+					<img
+						src={showClearImage ? tank : blurTank}
+						alt="tank"
+						style={{ width: "255px", height: "144px" }}
+					/>
 
-          {!showClearImage ? (
-            <button
-              className={styles.button}
-              onClick={() => setShowClearImage(!showClearImage)}
-            >
-              Відкрити
-            </button>
-          ) : (
-            <button
-              className={styles.button}
-              onClick={() => setShowClearImage(!showClearImage)}
-            >
-              Відкрито
-            </button>
-          )}
-        </div>
-      </ModalWind>
-    </div>
-  );
+					{!showClearImage ? (
+						<button className={styles.button} onClick={() => setShowClearImage(!showClearImage)}>
+							Відкрити
+						</button>
+					) : (
+						<button className={styles.button} onClick={() => setShowClearImage(!showClearImage)}>
+							Відкрито
+						</button>
+					)}
+				</div>
+			</ModalWind>
+		</div>
+	);
 };
 
 export default Weapons;
