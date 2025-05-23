3e7e223a839234a3c95c1b328f145ea3b69b0e56
Andrey <108063187+Minaht007@users.noreply.github.com>
2025-05-23 16:29:36 +0300

    first screen news

---
+++ src/component/news/mewsList.jsx
@@ -0,0 +1,38 @@
+import styles from "./news.module.css"
+import heardIcon from "../../assets/icons/heard.png"
+import nextPage from "../../assets/icons/nextPage.png"
+
+
+const newsCard = [
+    { title: "tank", url: "../../../src/assets/newsImg/tank.png", script: "100 користувачів розблокували озброєння ‘Танк’ сьогодні", count: "95" },
+    { title: "trasure", url: "../../../src/assets/newsImg/trasure.png", script: "10 000 одиниць допомоги було доставлено на фронт вчора", count: "75" },
+    { title: "air", url: "../../src/assets/newsImg/air.png", script: "100 користувачів розблокували озброєння Літак сьогодні", count: "45" }
+];
+
+
+
+const NewsList = () => {
+
+    return (
+
+        <div className={styles.newsListContainer}>
+            <ul>
+                {newsCard.map((item) => (
+                    <li key={item.title} className={styles.cardList}>
+                        <img src={item.url} alt="item.title" className={styles.img}/>
+                        <p className={styles.cardScript}>{item.script}</p>
+                        <button className={styles.btmHeard}>
+                            <img src={heardIcon} alt="heardIcon" width={21} height={18} />
+                            <p className={styles.count}>{item.count}</p>
+                        </button>
+                        <button className={styles.btmNextPage}>
+                            <img src={nextPage} alt="heardIcon" width={21} height={21}/>
+                        </button>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
+
+export default NewsList
